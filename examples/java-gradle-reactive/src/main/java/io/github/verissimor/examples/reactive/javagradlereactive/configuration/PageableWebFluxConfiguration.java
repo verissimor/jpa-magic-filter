9c63359a275e98977b4907bd5e29dc28b34025b7
Verissimo Ribeiro <vribeiro@landytech.com>
2022-08-26 22:56:09 +0100

    fix(reactive): force to use operator IN when there are comma

--- examples/java-gradle-reactive/src/main/java/io/github/verissimor/examples/reactive/javagradlereactive/configuration/PageableWebFluxConfiguration.java
+++ examples/java-gradle-reactive/src/main/java/io/github/verissimor/examples/reactive/javagradlereactive/configuration/PageableWebFluxConfiguration.java
@@ -2,6 +2,7 @@ package io.github.verissimor.examples.reactive.javagradlereactive.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
 
@@ -11,5 +12,13 @@ public class PageableWebFluxConfiguration implements WebFluxConfigurer {
     @Override
     public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
         configurer.addCustomResolver(new ReactivePageableHandlerMethodArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4400/") // only for tests
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*");
     }
 }
